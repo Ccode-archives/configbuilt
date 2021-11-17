2a5dcfcfc78aa7f597a1f6ef6ce43dfd8c7bd569
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-11-16 20:25:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ cd ~
 [ ! -d .configbuilt ] && mkdir .configbuilt
 cd .configbuilt
 wget https://raw.githubusercontent.com/Ccode-lang/angledat/main/angledat.py
-cp $dir/configbuilt.py .
+cp $direct/configbuilt.py .
