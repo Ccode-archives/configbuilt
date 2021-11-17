919764309fbd5df2c3803e418d0bd15925aaf675
Ccode-lang <78437178+Ccode-lang@users.noreply.github.com>
2021-11-16 20:06:12 -0700

    hopefully works

--- configbuilt.py
+++ configbuilt.py
@@ -2,6 +2,10 @@ import os
 import platform
 
 
+def is_tool(name):
+    return distutils.spawn.find_executable(name) is not None
+
+
 if platform.system() == "Windows":
     raise OSError("Don't run on Windows!")
 
@@ -19,3 +23,29 @@ if os.path.exists("build.andat"):
     data = angledat.read_dict("build.andat")
 else:
     raise OSError("Build data does not exist")
+
+if not istool("wget"):
+    raise OSError("wget not installed")
+try:
+    script = data["script to run"]
+except:
+    rasie OSError("No script to run given")
+
+if not os.path.exists("downloads"):
+    os.system("touch downloads")
+os.chdir("downloads")
+for name, value in data:
+    if name == "script to run":
+        continue
+    else:
+        if not os.path.exists(name):
+            os.system("mkdir" + name)
+        else:
+            raise OSError("Package '" + name + "' has already been downloaded")
+        try:
+            response = requests.get(value)
+            os.system("wget " + value)
+            os.system("mv " + value.split("/")[-1] + " '" + name + "/" + value + "'")
+        except requests.ConnectionError as exception:
+            print("Warning: Package '" + "' does not exist!)
+            os.remdir(name)
